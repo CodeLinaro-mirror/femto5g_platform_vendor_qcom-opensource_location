UBERCAF/vendor_qcom_opensource_location

Gnss Measurement fix for last message
Fixed an issue when Gnss measurements were
not sent to HAL if the last QMI message in
the sequence didn't have information for
any satellite

CRs-fixed: 1107676

Change-Id: Ie5fc6746eca558cf08bba855e97c9a1495ddeb8b

loc_api/loc_api_v02/LocApiV02.cpp
@@ -3416,19 +3416,21 @@ void LocApiV02 :: reportGnssMeasurementData(
                 gnss_measurement_report_ptr.system);
             meas_index++;
         }
-        // the GPS clock time reading
-        if (gnss_measurement_report_ptr.maxMessageNum ==
-            gnss_measurement_report_ptr.seqNum) {
+    }
+    else {
+        LOC_LOGE("%s:%d]: There is no GNSS measurement.\n",
+            __func__, __LINE__);
+    }
+    // the GPS clock time reading
+    if (gnss_measurement_report_ptr.maxMessageNum ==
+         gnss_measurement_report_ptr.seqNum &&
+         meas_index > 0) {
 
-            convertGnssClock(gnssMeasurementData.clock,
+         convertGnssClock(gnssMeasurementData.clock,
                 gnss_measurement_report_ptr);
 
-            // calling the base
-            LocApiBase::reportGnssMeasurementData(gnssMeasurementData);
-        }
-    } else {
-        LOC_LOGE("%s:%d]: There is no GNSS measurement.\n",
-                  __func__, __LINE__);
+         // calling the base
+         LocApiBase::reportGnssMeasurementData(gnssMeasurementData);
     }
 }
 

CodeLinaro-mirror/femto5g_platform_vendor_qcom-opensource_location

loc_api/loc_api_v02/LocApiV02.cpp
@@ -3456,21 +3456,23 @@ void LocApiV02 :: reportGnssMeasurementData(
                 gnss_measurement_report_ptr.system);
             meas_index++;
         }
-        // the GPS clock time reading
-        if (gnss_measurement_report_ptr.maxMessageNum ==
-            gnss_measurement_report_ptr.seqNum) {
+    }
+    else {
+        LOC_LOGE("%s:%d]: There is no GNSS measurement.\n",
+            __func__, __LINE__);
+    }
+    // the GPS clock time reading
+    if (gnss_measurement_report_ptr.maxMessageNum ==
+            gnss_measurement_report_ptr.seqNum &&
+            meas_index > 0) {
 
-            convertGnssClock(measurementsNotify.clock,
+        convertGnssClock(measurementsNotify.clock,
                 gnss_measurement_report_ptr);
 
-            // calling the base
-            LOC_LOGV ("%s:%d]: calling LocApiBase::reportGnssMeasurementData.\n",
-                    __func__, __LINE__);
-            LocApiBase::reportGnssMeasurementData(measurementsNotify);
-        }
-    } else {
-        LOC_LOGE("%s:%d]: There is no GNSS measurement.\n",
-                  __func__, __LINE__);
+        // calling the base
+        LOC_LOGV ("%s:%d]: calling LocApiBase::reportGnssMeasurementData.\n",
+                __func__, __LINE__);
+        LocApiBase::reportGnssMeasurementData(measurementsNotify);
     }
 }
 