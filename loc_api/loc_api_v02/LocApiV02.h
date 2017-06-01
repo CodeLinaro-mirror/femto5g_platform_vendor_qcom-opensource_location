778d563fc1e64d15c2abf0099bead5b8ae43334a
Mike Cailean <mcailean@codeaurora.org>
2017-06-01 13:18:18 -0700

    Handle updating the carrier configuration
    Set GPS_LOCK for cases where the modem has
    two SIMs.
    
    Change-Id: I9695e91a26ce63bf3bac75366d25b71be4f10b98
    CRs-fixed: 2018029

--- loc_api/loc_api_v02/LocApiV02.h
+++ loc_api/loc_api_v02/LocApiV02.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2018, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -84,12 +84,16 @@ private:
   bool mEngineOn;
   bool mMeasurementsStarted;
   std::vector<Resender> mResenders;
+  bool mIsMasterRegistered;
 
   /* Convert event mask from loc eng to loc_api_v02 format */
   static locClientEventMaskType convertMask(LOC_API_ADAPTER_EVENT_MASK_T mask);
 
-  /* Convert GPS LOCK mask */
-  static qmiLocLockEnumT_v02 convertGpsLockMask(GnssConfigGpsLock lock);
+  /* Convert GPS LOCK from LocationAPI format to QMI format */
+  static qmiLocLockEnumT_v02 convertGpsLockFromAPItoQMI(GnssConfigGpsLock lock);
+
+  /* Convert GPS LOCK from QMI format to LocationAPI format */
+  static GnssConfigGpsLock convertGpsLockFromQMItoAPI(qmiLocLockEnumT_v02 lock);
 
   /* Convert error from loc_api_v02 to loc eng format*/
   static enum loc_api_adapter_err convertErr(locClientStatusEnumType status);
@@ -174,6 +178,19 @@ private:
   void registerEventMask(LOC_API_ADAPTER_EVENT_MASK_T adapterMask);
   locClientEventMaskType adjustMaskIfNoSession(locClientEventMaskType qmiMask);
   bool cacheGnssMeasurementSupport();
+  void registerMasterClient();
+  int getGpsLock(uint8_t subType);
+
+  inline bool checkRegisterMaster() {
+      if (!mIsMasterRegistered) {
+          if (true == isMaster()) {
+              registerMasterClient();
+              mIsMasterRegistered = true;
+              return true;
+          }
+      }
+      return false;
+  }
 
 protected:
   virtual enum loc_api_adapter_err
@@ -277,11 +294,10 @@ public:
   virtual LocationError getGnssEnergyConsumed();
 
   /*
-    Returns
-    Current value of GPS Lock on success
-    -1 on failure
+  Returns
+  Current value of GPS Lock on success
+  -1 on failure
   */
-  virtual int getGpsLock(void);
   virtual int setSvMeasurementConstellation(const locClientEventMaskType mask);
   virtual LocationError setXtraVersionCheckSync(uint32_t check);
   virtual void installAGpsCert(const LocDerEncodedCertificate* pData,
