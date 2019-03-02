832efc9b4b94442e57940b55592b4a46a6165971
Kevin Tang <zhikait@codeaurora.org>
2019-03-01 18:16:16 -0800

    adding loc-multinet library
    This library handles multinet DNS and possibly other related
    setup calls.
    
    Change-Id: Id00d03e828c9f9c8e1d482c28f0eefdbe3845456
    CRs-Fixed: 2411953

---
+++ utils/multinet/LocMultinet.cpp
@@ -0,0 +1,56 @@
+/* Copyright (c) 2019 The Linux Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of The Linux Foundation, nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#define LOG_TAG "LocSvc_Multinet"
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <log_util.h>
+
+const char* resolvePublicUrl(const char* url) {
+    const char* ip = nullptr;
+
+    if (nullptr != url) {
+        struct hostent *he = gethostbyname(url);
+        if (nullptr != he) {
+            struct in_addr* addr = (struct in_addr*)(he->h_addr_list[0]);
+            ip = inet_ntoa(*addr);
+        } else {
+            LOC_LOGw("can not resolve %s", url);
+        }
+    } else {
+        LOC_LOGw("recived a null url");
+    }
+
+    return ip;
+}
+
