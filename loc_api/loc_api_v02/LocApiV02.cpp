f2c0e6e371f2cdfd69e8304e5ecc5d1c13dccaaa
Yan Huang <quic_huang@quicinc.com>
2020-01-07 14:02:29 -0800

    time reference delta calculation is wrong
    time reference is of boottime clock. Currently we are using
    uptimemillis() for current time, but that is of a different
    clock (monotonic). So this time is going to be wrong. Also
    the monotonic clock stops ticking when device is in suspend
    mode. So it is not a good choice of clock anyways.
    
    Change-Id: Ia9dd22345329bb8e56492310088f9cab2727e6ce
    CRs-Fixed: 2598670

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -29,7 +29,7 @@
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 
-Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -975,7 +975,7 @@ void LocApiV02 ::
 
   inject_time_msg.timeUtc = time;
 
-  inject_time_msg.timeUtc += (int64_t)(uptimeMillis() - timeReference);
+  inject_time_msg.timeUtc += (int64_t)(elapsedRealtime() - timeReference);
 
   inject_time_msg.timeUnc = uncertainty;
 
