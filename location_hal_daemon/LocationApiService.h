a18b70eb549c832c5f1f707cf7483747a3c2b14e
Wei (GPS SD) Chen <weic@codeaurora.org>
2021-07-16 10:08:11 -0700

    LCA/LIA support for integrity risk level
    LIA: API to set the integrity risk level for protection cal
    LCA: output protection levels in PVT report
    
    Change-Id: I7347d940ec9c871cb116445cf144cbfcdc7a955d
    CRs-fixed: 2993619

--- location_hal_daemon/LocationApiService.h
+++ location_hal_daemon/LocationApiService.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2018-2020 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2018-2021 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -242,6 +242,7 @@ private:
     void configUserConsentTerrestrialPositioning(
             LocConfigUserConsentTerrestrialPositioningReqMsg* pMsg);
     void configOutputNmeaTypes(const LocConfigOutputNmeaTypesReqMsg* pMsg);
+    void configEngineIntegrityRisk(const LocConfigEngineIntegrityRiskReqMsg* pMsg);
 
     // Location configuration API get/read requests
     void getGnssConfig(const LocAPIMsgHeader* pReqMsg,
