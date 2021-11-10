13a1a9c1300c04d7f808711d83fec9c0644d44fd
Hoss Zhou <quic_zhouh@quicinc.com>
2021-11-10 14:24:18 +0800

    add LOC_POS_TECH_MASK_PROPAGATED bit implementation
    Change-Id: Ibd3940d6a87114a54bbce294a7c291efd7cb4987
    CRs-Fixed: 3098384

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -8299,6 +8299,9 @@ LocPosTechMask LocApiV02 :: convertPosTechMask(
    if (mask & QMI_LOC_POS_TECH_MASK_PDR_V02)
       locTechMask |= LOC_POS_TECH_MASK_PDR;
 
+   if (mask & QMI_LOC_POS_TECH_MASK_PROPAGATED_V02)
+      locTechMask |= LOC_POS_TECH_MASK_PROPAGATED;
+
    return locTechMask;
 }
 
