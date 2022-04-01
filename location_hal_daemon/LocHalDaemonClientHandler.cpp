fc9362731a6d61e026a47ffbbfa0360b6ebb7547
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2022-04-01 11:52:51 -0700

    Location integration api: fix lia command failure
    Scenario 1: lia command is issued before communication
        has been established with hal daemon
    Scenario 2: lia command is in progress while hal daemon
                crashed and resetarted
    
    CRs-fixed: 3166221
    
    Change-Id: I3d62326fc31a7a4c22bb4fcd8cc34a21c7338cdb

--- location_hal_daemon/LocHalDaemonClientHandler.cpp
+++ location_hal_daemon/LocHalDaemonClientHandler.cpp
@@ -25,7 +25,41 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+Changes from Qualcomm Innovation Center are provided under the following license:
 
+Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted (subject to the limitations in the
+disclaimer below) provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include <cinttypes>
 #include <gps_extended_c.h>
 #include <LocationApiMsg.h>
@@ -1127,6 +1161,27 @@ void LocHalDaemonClientHandler::onGnssEnergyConsumedInfoAvailable(
         } else {
             LOC_LOGe("LocAPIGnssEnergyConsumedIndMsg serializeToProtobuf failed");
         }
+    }
+}
+
+void LocHalDaemonClientHandler::sendCapabilitiesMsg() {
+    LOC_LOGd("--< sendCapabilitiesMsg=0x%" PRIx64, mCapabilityMask);
+
+    if ((nullptr != mIpcSender) && (0 != mCapabilityMask)) {
+        string pbStr;
+        LocAPICapabilitiesIndMsg msg(SERVICE_NAME, mCapabilityMask, &mService->mPbufMsgConv);
+        if (msg.serializeToProtobuf(pbStr)) {
+            bool rc = sendMessage(pbStr.c_str(), pbStr.size(), msg.msgId);
+            // purge this client if failed
+            if (!rc) {
+                LOC_LOGe("failed rc=%d purging client=%s", rc, mName.c_str());
+                mService->deleteClientbyName(mName);
+            }
+        } else {
+            LOC_LOGe("LocAPICapabilitiesIndMsg serializeToProtobuf failed");
+        }
+    } else {
+        LOC_LOGe("mIpcSender is NULL or mCapabilityMask is %d", mCapabilityMask);
     }
 }
 
--- location_hal_daemon/LocationApiService.cpp
+++ location_hal_daemon/LocationApiService.cpp
@@ -25,7 +25,41 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+Changes from Qualcomm Innovation Center are provided under the following license:
 
+Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted (subject to the limitations in the
+disclaimer below) provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #include <stdint.h>
 #include <sys/stat.h>
 #include <dlfcn.h>
@@ -712,22 +746,22 @@ void LocationApiService::newClient(LocAPIClientRegisterReqMsg *pMsg) {
     std::lock_guard<std::mutex> lock(mMutex);
     std::string clientname(pMsg->mSocketName);
 
-    // if this name is already used return error
-    if (mClients.find(clientname) != mClients.end()) {
-        LOC_LOGe("invalid client=%s already existing", clientname.c_str());
-        return;
+    // if this name is already used, we inform client of the capability
+    // to allow callflow to continue on client side
+    LocHalDaemonClientHandler* pClient = getClient(clientname);
+    if (pClient) {
+        LOC_LOGi("client=%s already exists, send capability", clientname.c_str());
+        pClient->sendCapabilitiesMsg();
+    } else {
+        // store it in client property database
+        pClient = new LocHalDaemonClientHandler(this, clientname, pMsg->mClientType);
+        if (!pClient) {
+            LOC_LOGe("failed to register client=%s", clientname.c_str());
+            return;
+        }
+        mClients.emplace(clientname, pClient);
+        LOC_LOGi(">-- registered new client=%s", clientname.c_str());
     }
-
-    // store it in client property database
-    LocHalDaemonClientHandler *pClient =
-            new LocHalDaemonClientHandler(this, clientname, pMsg->mClientType);
-    if (!pClient) {
-        LOC_LOGe("failed to register client=%s", clientname.c_str());
-        return;
-    }
-
-    mClients.emplace(clientname, pClient);
-    LOC_LOGi(">-- registered new client=%s", clientname.c_str());
 }
 
 void LocationApiService::deleteClient(LocAPIClientDeregisterReqMsg *pMsg) {
