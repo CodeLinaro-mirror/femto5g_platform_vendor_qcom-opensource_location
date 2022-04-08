c6c14dc2df26e18c651aa4aecfbd46c4586910ff
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2022-04-08 16:18:28 -0700

    Location client api: handle multiple capability callback
    for capability update, do not restart the session
    and reset the single shot session status
    
    Change-Id: Ic6f3d6bdd7c13be38c6c2e8d170f8f84bb6b41a1
    CRs-fixed: 3170571

--- client_api/src/LocationClientApiImpl.cpp
+++ client_api/src/LocationClientApiImpl.cpp
@@ -2287,6 +2287,7 @@ LocationClientApiImpl - LocIpc onReceive handler
 ******************************************************************************/
 void LocationClientApiImpl::capabilitesCallback(ELocMsgID msgId, const void* msgData) {
 
+    bool oldHalRegisterd = mHalRegistered;
     mHalRegistered = true;
     const LocAPICapabilitiesIndMsg* pCapabilitiesIndMsg =
             (LocAPICapabilitiesIndMsg*)(msgData);
@@ -2309,6 +2310,11 @@ void LocationClientApiImpl::capabilitesCallback(ELocMsgID msgId, const void* msg
         } else {
             LOC_LOGe("LocAPIUpdateCallbacksReqMsg serializeToProtobuf failed");
         }
+    }
+
+    if (oldHalRegisterd == true) {
+        LOC_LOGi("hal is not restarted, return");
+        return;
     }
 
     LOC_LOGe(">>> session id %d, cap mask 0x%" PRIx64, mSessionId, mCapsMask);
