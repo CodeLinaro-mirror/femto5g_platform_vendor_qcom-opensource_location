2628ca6ec83287ae8425c81c006b0c2d46a8962c
Yan Huang <quic_huang@quicinc.com>
2022-05-03 12:31:04 -0700

    Clear QMI_LOC_EVENT_MASK_GNSS_EVENT_REPORT_V02 mask
    To avoid wake up from QMI_LOC_EVENT_REPORT_IND
    
    Change-Id: I1d2d3af686cf8c8adc9dbee4fa10f4b443a5a976
    CRs-Fixed: 3171840

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -645,6 +645,11 @@ locClientEventMaskType LocApiV02 :: adjustMaskIfNoSession(locClientEventMaskType
                                            QMI_LOC_EVENT_MASK_EPHEMERIS_REPORT_V02 |
                                            QMI_LOC_EVENT_MASK_GNSS_EVENT_REPORT_V02 |
                                            QMI_LOC_EVENT_MASK_NEXT_LS_INFO_REPORT_V02;
+        // clear GNSS_EVENT_REPORT mask because QMI_LOC_EVENT_MASK_FEATURE_STATUS_V02 is set
+        // when LOC_SUPPORTED_FEATURE_DYNAMIC_FEATURE_STATUS is supported
+        if (ContextBase::isFeatureSupported(LOC_SUPPORTED_FEATURE_DYNAMIC_FEATURE_STATUS)) {
+            clearMask |= QMI_LOC_EVENT_MASK_GNSS_EVENT_REPORT_V02;
+        }
         qmiMask = qmiMask & ~clearMask;
     }
     LOC_LOGd("oldQmiMask=%" PRIu64 " qmiMask=%" PRIu64 " mInSession: %d",
@@ -2574,6 +2579,10 @@ locClientEventMaskType LocApiV02 :: convertMask(
 
   if (mask & LOC_API_ADAPTER_BIT_GNSS_NHZ_MEASUREMENT) {
       eventMask |= QMI_LOC_EVENT_MASK_GNSS_NHZ_MEASUREMENT_REPORT_V02;
+  }
+
+  if (mask & LOC_API_ADAPTER_BIT_FEATURE_STATUS_UPDATE) {
+      eventMask |= QMI_LOC_EVENT_MASK_FEATURE_STATUS_V02;
   }
 
   return eventMask;
