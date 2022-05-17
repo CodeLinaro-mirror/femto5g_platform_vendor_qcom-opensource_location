534095830b9dcfb2ac6b4a088270084f8c68b16a
Naresh Munagala <quic_nareshm@quicinc.com>
2022-05-17 11:19:09 +0530

    log level changes for location hal daemon
    Change-Id: I7b36653a84189967f152b22cc875e70db13a5dc8
    CRs-Fixed: 3198599

--- location_hal_daemon/LocationApiService.cpp
+++ location_hal_daemon/LocationApiService.cpp
@@ -1521,7 +1521,7 @@ void LocationApiService::onControlResponseCallback(LocationError err, uint32_t s
         mConfigReqs.erase(configReqData);
         LOC_LOGd("--< map size %d", mConfigReqs.size());
     } else {
-        LOC_LOGe("--< client not found for session id %d", sessionId);
+        LOC_LOGw("--< client not found for session id %d", sessionId);
     }
 }
 
@@ -1548,7 +1548,7 @@ void LocationApiService::onControlCollectiveResponseCallback(
         mConfigReqs.erase(configReqData);
         LOC_LOGd("--< map size %d", mConfigReqs.size());
     } else {
-        LOC_LOGe("--< client not found for session id %d", sessionId);
+        LOC_LOGw("--< client not found for session id %d", sessionId);
     }
 }
 
