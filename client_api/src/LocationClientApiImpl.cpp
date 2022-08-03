0d04eb69292726de4f4b5ec701c2b9c14b08f1a0
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2022-08-03 11:03:47 -0700

    Revert "Location client API: ues only Gnss stack for time-based session"
    This reverts commit 8bed8f7a04673026ce0df2eb3b19b284fb4ce584.
    And make LCA stack on VT side in sync with MDM side.
    
    Change-Id: I6e84717a80ff7cf4c68deef7aea18b8690388a9a
    CRs-Fixed: 2912212

--- client_api/src/LocationClientApiImpl.cpp
+++ client_api/src/LocationClientApiImpl.cpp
@@ -1422,14 +1422,10 @@ void LocationClientApiImpl::updateCallbacks(LocationCallbacks& callbacks) {
     //convert callbacks to callBacksMask
     LocationCallbacksMask callBacksMask = 0;
     if (callbacks.trackingCb) {
-        callBacksMask |= E_LOC_CB_DISTANCE_BASED_TRACKING_BIT;
+        callBacksMask |= E_LOC_CB_TRACKING_BIT;
     }
     if (callbacks.gnssLocationInfoCb) {
-        if (mLocationCb) {
-            callBacksMask |= E_LOC_CB_SIMPLE_LOCATION_INFO_BIT;
-        } else {
-            callBacksMask |= E_LOC_CB_GNSS_LOCATION_INFO_BIT;
-        }
+        callBacksMask |= E_LOC_CB_GNSS_LOCATION_INFO_BIT;
     }
     if (callbacks.engineLocationsInfoCb) {
         callBacksMask |= E_LOC_CB_ENGINE_LOCATIONS_INFO_BIT;
@@ -2544,10 +2540,8 @@ void IpcListener::onReceive(const char* data, uint32_t length,
                 }
                 LocAPILocationIndMsg msg(sockName.c_str(), pbLocApiLocIndMsg,
                         &mApiImpl.mPbufMsgConv);
-                LocationCallbacksMask tempMask =
-                        (E_LOC_CB_DISTANCE_BASED_TRACKING_BIT | E_LOC_CB_SIMPLE_LOCATION_INFO_BIT);
                 if ((mApiImpl.mSessionId != LOCATION_CLIENT_SESSION_ID_INVALID) &&
-                        (mApiImpl.mCallbacksMask & tempMask)) {
+                        (mApiImpl.mCallbacksMask & E_LOC_CB_TRACKING_BIT)) {
                     const LocAPILocationIndMsg* pLocationIndMsg = (LocAPILocationIndMsg*)(&msg);
                     Location location = parseLocation(pLocationIndMsg->locationNotification);
                     if (mApiImpl.mLocationCb) {
