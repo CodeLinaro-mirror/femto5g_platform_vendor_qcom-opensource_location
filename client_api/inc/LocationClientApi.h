d1d0a76d2a054b9593d01420c9d5ec8bb4ac1983
Yan Huang <quic_huang@quicinc.com>
2022-08-31 17:41:04 -0700

    GNSS at tunnel entrance
    Set LocationTechnologyMask with LOCATION_TECHNOLOGY_PROPAGATED_BIT
    when position has  less than 3 refreshed SVs per constellation.
    This mask indicates that the position is inside a tunnel.
    
    Change-Id: I84f6c722b4b76c02475e76eb220553b4a20f247d
    CRs-Fixed: 3279105

--- client_api/inc/LocationClientApi.h
+++ client_api/inc/LocationClientApi.h
@@ -26,6 +26,42 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+Changes from Qualcomm Innovation Center are provided under the following license:
+
+Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted (subject to the limitations in the
+disclaimer below) provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef LOCATIONCLIENTAPI_H
 #define LOCATIONCLIENTAPI_H
 
@@ -56,6 +92,12 @@
          LOCATION_TECHNOLOGY_HYBRID_BIT
 #define LOCATION_POS_TECH_PPE_BIT \
          LOCATION_TECHNOLOGY_PPE_BIT
+#define LOCATION_POS_TECH_VEH_BIT \
+         LOCATION_TECHNOLOGY_VEH_BIT
+#define LOCATION_POS_TECH_VIS_BIT \
+         LOCATION_TECHNOLOGY_VIS_BIT
+#define LOCATION_POS_TECH_PROPAGATED_BIT \
+         LOCATION_TECHNOLOGY_PROPAGATED_BIT
 // DEPRECATION - BACKWARD COMPATIBILITY SECTION
 
 using std::string;
@@ -179,7 +221,10 @@ enum LocationTechnologyMask {
     LOCATION_TECHNOLOGY_VEH_BIT                      = (1<<9),
     /** Visual data was used to calculate
      *  Location. <br/>   */
-    LOCATION_TECHNOLOGY_VIS_BIT                      = (1<<10)
+    LOCATION_TECHNOLOGY_VIS_BIT                      = (1<<10),
+    /** Propagation logic was used to calculate
+     *  Location. <br/>   */
+    LOCATION_TECHNOLOGY_PROPAGATED_BIT               = (1<<11)
 };
 
 /** Specify the set of navigation solutions that contribute
