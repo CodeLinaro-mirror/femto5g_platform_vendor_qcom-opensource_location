4f0e4a9537f8f29a1a168321e4af4619a2c0c6d8
Nilesh Gharde <quic_ngharde@quicinc.com>
2023-09-14 22:13:56 -0700

    Clean up in adjustLocClientEventMask
    Engine state ON/OFF would be recevied when
    tracking session starts/stops resp. We already
    setting/resetting the NMEA mask bit when session starts/stops.
    There is no need to reset the NEMA bit on engine OFF.
    
    CRs-fixed: 3612230
    Change-Id: Ie2d175dadf04d8bf2569e339a32de675962e067c
    (cherry picked from commit a37b3b56f9bfa498fe197ae5d032b0d5decd29dd)

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -700,9 +700,6 @@ locClientEventMaskType LocApiV02 :: adjustLocClientEventMask(locClientEventMaskT
             clearMask |= QMI_LOC_EVENT_MASK_GNSS_EVENT_REPORT_V02;
         }
         qmiMask = qmiMask & ~clearMask;
-    } else if (!mEngineOn) {
-        locClientEventMaskType clearMask = QMI_LOC_EVENT_MASK_NMEA_V02;
-        qmiMask = qmiMask & ~clearMask;
     }
 
     //Enable feature status report for master client
@@ -4823,9 +4820,11 @@ void LocApiV02 :: reportEngineState (
                   resender();
               }
               mpLocApiV02->mResenders.clear();
+              // update the registration mask upon receiving Engine state off
+              // as we have already flushed out the queue, and can un-register
+              // the ENGINE_STATE event
+              mpLocApiV02->registerEventMask();
           }
-          // update the registration mask upon receiving Engine state
-          mpLocApiV02->registerEventMask();
       }
   };
 
