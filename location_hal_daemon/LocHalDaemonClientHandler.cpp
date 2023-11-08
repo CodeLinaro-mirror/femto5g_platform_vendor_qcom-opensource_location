8a42c06fde431eb2962a6bd23a7b31f5b0fedc96
Yan Huang <quic_huang@quicinc.com>
2023-11-08 15:14:31 -0800

    LCA/LIA stress test issues
    1. Memory leak of listener thread
    2. Message is received when the Impl in being destroyed
    
    Change-Id: Ib4950afdbcd6601cb7f98b0b869141d35d59df5f
    CRs-Fixed: 3673355
    (cherry picked from commit 6fe7d10362445328302c5626a7fc7b565bb01e7b)
    (cherry picked from commit cc9e2a11ddea9adfce8a9ebd9cc7327e64dc155c)

--- location_hal_daemon/LocHalDaemonClientHandler.cpp
+++ location_hal_daemon/LocHalDaemonClientHandler.cpp
@@ -444,7 +444,7 @@ void LocHalDaemonClientHandler::pingTest() {
     }
 }
 
-void LocHalDaemonClientHandler::cleanup() {
+void LocHalDaemonClientHandler::cleanup(bool forceRemove) {
     // please do not attempt to hold the lock, as the caller of this function
     // already holds the lock
 
@@ -452,7 +452,7 @@ void LocHalDaemonClientHandler::cleanup() {
     // remote client that is no longer reachable
     mIpcSender = nullptr;
 
-    if (0 != remove(mName.c_str())) {
+    if (forceRemove && 0 != remove(mName.c_str())) {
         LOC_LOGw("<-- failed to remove file %s error %s", mName.c_str(), strerror(errno));
     }
 
--- location_hal_daemon/LocationApiService.h
+++ location_hal_daemon/LocationApiService.h
@@ -185,7 +185,7 @@ public:
 #endif
 
     // other APIs
-    void deleteClientbyName(const std::string name);
+    void deleteClientbyName(const std::string name, bool forceRemove = true);
     void deleteEapClientByIds(int id1, int id2);
 
     // protobuf conversion util class
