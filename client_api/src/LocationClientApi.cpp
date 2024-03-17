bc1836768e1ea241b844d263c73fe032ac6654ed
Yan Huang <quic_huang@quicinc.com>
2024-03-17 12:56:47 -0700

    Add session status in LCA Location
    To report session status in LCA
    Location data struct
    
    Change-Id: I7073bb2c66ddec4215f6d43f5231d99ffddf6b16
    CRs-Fixed: 3761369

--- client_api/src/LocationClientApi.cpp
+++ client_api/src/LocationClientApi.cpp
@@ -940,7 +940,13 @@ DECLARE_TBL(LocationFlagsMask) = {
     {LOCATION_HAS_VERTICAL_ACCURACY_BIT, "VERT_ACCURACY"},
     {LOCATION_HAS_SPEED_ACCURACY_BIT, "SPEED_ACCURACY"},
     {LOCATION_HAS_BEARING_ACCURACY_BIT, "BEARING_ACCURACY"},
-    {LOCATION_HAS_BEARING_ACCURACY_BIT, "TS"}
+    {LOCATION_HAS_TIMESTAMP_BIT, "TS"},
+    {LOCATION_HAS_ELAPSED_REAL_TIME_BIT, "ELAPSED_REAL_TIME"},
+    {LOCATION_HAS_ELAPSED_REAL_TIME_UNC_BIT, "ELAPSED_REAL_TIME_UNC"},
+    {LOCATION_HAS_TIME_UNC_BIT, "TIME_UNC"},
+    {LOCATION_HAS_GPTP_TIME_BIT, "GPTP_TIME"},
+    {LOCATION_HAS_GPTP_TIME_UNC_BIT, "GPTP_TIME_UNC"},
+    {LOCATION_HAS_SESSION_STATUS_BIT, "SESSION_STATUS"},
 };
 // LocationTechnologyMask
 DECLARE_TBL(LocationTechnologyMask) = {
@@ -1421,7 +1427,7 @@ string LLAInfo::toString() const {
 
 string Location::toString() const {
     string out;
-    out.reserve(256);
+    out.reserve(512);
 
     out += FIELDVAL_MASK(flags, LocationFlagsMask_tbl);
     out += FIELDVAL_DEC(timestamp);
@@ -1437,7 +1443,7 @@ string Location::toString() const {
     out += FIELDVAL_MASK(techMask, LocationTechnologyMask_tbl);
     out += FIELDVAL_DEC(elapsedgPTPTime);
     out += FIELDVAL_DEC(elapsedgPTPTimeUnc);
-
+    out += FIELDVAL_MASK(sessionStatus, LocSessionStatus_tbl);
     return out;
 }
 
