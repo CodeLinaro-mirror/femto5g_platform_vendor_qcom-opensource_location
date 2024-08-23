cf6a27a6a6237cc888392a8df0f08b06d19a4c13
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2024-08-23 13:15:39 -0700

    GNSS measurement report changes for BDS preferred and only cases
    1: report clock info based on GPS system
    2: report ISB by retrieving time bias and its unc for gpsL1 from BDS
       clk info ahead of time, so ISB that uses gpsL1 and its unc can be
       calculated
    
    CRs-fixed: 3922158
    
    Change-Id: I4fed466037548521678191b66d56233686882848

--- loc_api/loc_api_v02/LocApiV02.h
+++ loc_api/loc_api_v02/LocApiV02.h
@@ -208,7 +208,6 @@ private:
   std::vector<Resender> mResenders;
   bool mMasterRegisterNotSupported;
   GnssMeasurements*  mGnssMeasurements;
-  bool mPreferredSignalTypeReceived;
   int  mMsInWeek;
   bool mAgcIsPresent;
   bool mIsFirstFinalFixReported;
@@ -222,7 +221,6 @@ private:
   qmiLocPlatformPowerStateEnumT_v02 mPlatformPowerState;
   bool mIsFullTracking;
   qmiLocGnssSignalTypeMaskT_v02 mPreferredSignalType;
-  referenceSignalTypeForIsb mReferenceSignalTypeForIsb;
   GnssMeasurementsNotification m1HzMeasurementsNotify;
 
   /* Convert event mask from loc eng to loc_api_v02 format */
@@ -370,14 +368,11 @@ private:
                sizeof(GnssSvMeasurementHeader);
       }
       memset(&mTimeBiases, 0, sizeof(mTimeBiases));
-      mPreferredSignalTypeReceived = false;
       mMsInWeek = -1;
       mAgcIsPresent = false;
   }
 
   void setGnssBiasesForL1CA();
-  void setGnssBiasesForB1I();
-  void setGnssBiases();
   /* convert and report ODCPI request */
   void requestOdcpi(
     const qmiLocEventWifiReqIndMsgT_v02& odcpiReq);
