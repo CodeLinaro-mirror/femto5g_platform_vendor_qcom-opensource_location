a1a054f4cd177cdfd7da5938b9c9b0945dc1ca8c
Yan Huang <quic_huang@quicinc.com>
2024-08-25 17:31:14 -0700

    LIA: Support Configuration for updateNetworkInfo
    This API is designed for scenarios where the network
    is brought up by the external customer.
    1. Network connection status
    2. Network type
    3. MCCMNC for Xtra User Agent string
    4. Country for Xtra server URL
    
    Change-Id: I1b82ef12f2724880a2ffcf6975b7e0a4e6f6f7d5
    CRs-Fixed: 3982735

--- client_api_testapp/main.cpp
+++ client_api_testapp/main.cpp
@@ -26,12 +26,17 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
-
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 SPDX-License-Identifier: BSD-3-Clause-Clear
 */
+
+/*
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,6 +55,7 @@ SPDX-License-Identifier: BSD-3-Clause-Clear
 #include <loc_misc_utils.h>
 #include <thread>
 #include <unordered_map>
+#include <iostream>
 #include <algorithm>
 
 #include <LocationClientApi.h>
@@ -160,7 +166,7 @@ enum TrackingSessionType {
 #define MODIFY_GEOFENCES            "modifyGeofences"
 #define REMOVE_GEOFENCES            "removeGeofences"
 #define SET_XTRA_END_USER_CONSENT   "setXtraEndUserConsent"
-
+#define SET_NETWORK_INFO            "setNetworkInfo"
 
 static bool openPort(void)
 {
@@ -662,6 +668,7 @@ static void printHelp() {
             MODIFY_GEOFENCES );
     printf("%s: remove geofences with indexes\n", REMOVE_GEOFENCES );
     printf("%s: Set xtra end user consent \n", SET_XTRA_END_USER_CONSENT);
+    printf("%s: set external network info \n", SET_NETWORK_INFO );
 }
 
 void setRequiredPermToRunAsLocClient() {
@@ -2126,9 +2133,9 @@ int main(int argc, char *argv[]) {
     while (1) {
         bool retVal = true;
         char buf[1500];
-        std::string strBuf(buf);
         memset (buf, 0, sizeof(buf));
         fgets(buf, sizeof(buf), stdin);
+        std::string strBuf(buf);
 
         printf("execute command %s\n", buf);
         if (!pIntClient) {
@@ -2568,6 +2575,55 @@ int main(int argc, char *argv[]) {
             printf("xtrauserConsent %d\n", xtraUserConsent);
             if (pIntClient) {
                 pIntClient->setUserConsentForXtra(xtraUserConsent);
+            }
+        } else if (strBuf.compare(0, strlen(SET_NETWORK_INFO),
+                SET_NETWORK_INFO) == 0) {
+            if (pIntClient) {
+                NetworkInfoData nwData = {};
+                int32_t nwStatus = 0, nwType = 0;
+                std::cout << "Enter Connection Status  " << std::endl;
+                std::cout << "0 for Unknown, 1 for CONNECTED, 2 for DISCONNECTED: ";
+                std::cin >> nwStatus;
+                if (std::cin.fail()) {
+                    std::cerr << "Invalid input for Connection Status" << std::endl;
+                }
+                nwData.connection = static_cast<NetworkConnection>(nwStatus);
+
+                std::cout << "Enter Country: ";
+                std::cin.ignore(); // To ignore the newline character left in the buffer
+                std::getline(std::cin, nwData.country);
+                if (nwData.country.empty()) {
+                    std::cerr << "Invalid input for Country" << std::endl;
+                    nwData.country = " ";
+                }
+
+                std::cout << "Enter MccMnc: ";
+                std::getline(std::cin, nwData.mccmnc);
+                if (nwData.mccmnc.empty()) {
+                     std::cerr << "Invalid input for MccMnc" << std::endl;
+                     nwData.mccmnc = " ";
+                }
+
+                std::cout << "Enter Network Type (0 for UNKNOWN, 1 for WWAN, 2 for WLAN): ";
+                std::cin >> nwType;
+                if (std::cin.fail()) {
+                     std::cerr << "Invalid input for Network Type" << std::endl;
+                }
+                nwData.networkType = static_cast<NetworkType>(nwType);
+
+                // Output the collected data
+                std::cout << "Network Info:" << std::endl;
+                std::cout << "Connection Status: "
+                        << (nwData.connection == NET_CONNECTED ? "Connected"
+                        : (nwData.connection == NET_DISCONNECTED ? "Disconnected" : "Unknown"))
+                        << std::endl;
+                std::cout << "Country: " << nwData.country << std::endl;
+                std::cout << "MccMnc: " << nwData.mccmnc << std::endl;
+                std::cout << "Network Type: "
+                        << (nwData.networkType == TYPE_WWAN ? "WWAN"
+                        : (nwData.networkType == TYPE_WLAN ? "WLAN": "Unknown")) << std::endl;
+
+                pIntClient->updateNetworkInfo(nwData);
             }
         } else {
             int command = buf[0];
