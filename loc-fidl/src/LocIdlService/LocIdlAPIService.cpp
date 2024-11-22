a187ef27edc846bbf13164e9581d5e2677e1181f
Karan Singh Parmar <quic_kparmar@quicinc.com>
2024-11-22 15:55:04 +0530

    fidl v1.1 changes
    - add measurement code types
    - return FIDL version
    
    Change-Id: I2d50c6d10a6c560f72af3290fb59963545662716
    CRs-Fixed: 4001884

--- loc-fidl/src/LocIdlService/LocIdlAPIService.cpp
+++ loc-fidl/src/LocIdlService/LocIdlAPIService.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2024- 2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -988,7 +988,7 @@ void LocIdlAPIService::stopPositionSession
                         mLCAService->mDiagLogIface->diagLogSessionInfo(
                                 idlSessionInfo, mClient->hashCode());
                     }
-                    if (!mLCAService->numControlRequests) {
+                    if (!mLCAService->numControlRequests && mLCAService->mEnableStopSession) {
                         LOC_LOGe(" Sending STOP Session request !!");
                         mLCAService->mLcaInstance->stopPositionSession();
                         posCount = 0;
@@ -1135,7 +1135,14 @@ int main() {
         pLCAService->init();
     }
 
+    static loc_param_s_type locIdlServiceConfEntryTable[] = {
+        {"ENABLE_LOC_IDL_SERVICE_STOP_SESSION", &pLCAService->mEnableStopSession, NULL, 'n'}
+    };
+
+    UTIL_READ_CONF(LOC_PATH_IZAT_CONF_STR, locIdlServiceConfEntryTable);
+
     if (gptpInit()) {
+        pLCAService->mIsGptpInitialized = true;
         LOC_LOGd(" GPTP init success ");
     } else {
         LOC_LOGe(" GPTP init failed ");
--- loc-fidl/src/LocIdlService/LocIdlAPIService.h
+++ loc-fidl/src/LocIdlService/LocIdlAPIService.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2024- 2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -189,6 +189,8 @@ public:
     /**Keeps track of latest caps recieved */
     uint32_t mGnssCapabilites;
 
+    /** Configuration varaible to control Stop session execution by LocIdlService */
+    bool mEnableStopSession = false;
 private:
     static LocIdlAPIService *mInstance;
     LocationClientApi* mLcaInstance;
