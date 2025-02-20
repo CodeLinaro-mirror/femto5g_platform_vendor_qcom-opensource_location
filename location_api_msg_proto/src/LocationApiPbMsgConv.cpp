820cc4188f3d89ba23ef5a244c333f07e038b59d
Naresh Munagala <quic_nareshm@quicinc.com>
2025-02-20 08:42:38 +0530

    add missing position tech mask parameters
    add missing postech mask for protobuf conversion.
    
    Change-Id: Ic0edfdf3842a87b0a41267bab34b7a690345fb28
    CRs-Fixed: 4061920

--- location_api_msg_proto/src/LocationApiPbMsgConv.cpp
+++ location_api_msg_proto/src/LocationApiPbMsgConv.cpp
@@ -1201,6 +1201,15 @@ uint32_t LocationApiPbMsgConv::getPBMaskForLocationTechnologyMask(
     if (locTechMask & LOCATION_TECHNOLOGY_PPE_BIT) {
         pbLocTechMask |= PB_LOCATION_TECHNOLOGY_PPE_BIT;
     }
+    if (locTechMask & LOCATION_TECHNOLOGY_VEH_BIT) {
+        pbLocTechMask |= PB_LOCATION_TECHNOLOGY_VEH_BIT;
+    }
+    if (locTechMask & LOCATION_TECHNOLOGY_VIS_BIT) {
+        pbLocTechMask |= PB_LOCATION_TECHNOLOGY_VIS_BIT;
+    }
+    if (locTechMask & LOCATION_TECHNOLOGY_PROPAGATED_BIT) {
+        pbLocTechMask |= PB_LOCATION_TECHNOLOGY_PROPAGATED_BIT;
+    }
     LocApiPb_LOGv("LocApiPB: locTechMask:%x, pbLocTechMask:%x", locTechMask, pbLocTechMask);
     return pbLocTechMask;
 }
@@ -2264,6 +2273,15 @@ uint32_t LocationApiPbMsgConv::getLocationTechnologyMaskFromPB(
     }
     if (pbLocTechMask & PB_LOCATION_TECHNOLOGY_PPE_BIT) {
         locTechMask |= LOCATION_TECHNOLOGY_PPE_BIT;
+    }
+    if (pbLocTechMask & PB_LOCATION_TECHNOLOGY_VEH_BIT) {
+        locTechMask |= LOCATION_TECHNOLOGY_VEH_BIT;
+    }
+    if (pbLocTechMask & PB_LOCATION_TECHNOLOGY_VIS_BIT) {
+        locTechMask |= LOCATION_TECHNOLOGY_VIS_BIT;
+    }
+    if (pbLocTechMask & PB_LOCATION_TECHNOLOGY_PROPAGATED_BIT) {
+        locTechMask |= LOCATION_TECHNOLOGY_PROPAGATED_BIT;
     }
     LocApiPb_LOGv("LocApiPB: pbLocTechMask:%x, locTechMask:%x", pbLocTechMask, locTechMask);
     return locTechMask;
