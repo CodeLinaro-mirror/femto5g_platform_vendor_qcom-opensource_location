cde05882ea01d070c0142764db1f3ae561ea6f4a
Karan Singh Parmar <kparmar@qti.qualcomm.com>
2025-03-12 15:07:31 -0700

    Fix segmenat fault due to callback after LCA or LIA object being destructed
    Add LocClientDestroyCb and LocIntegrationDestroyCb
    1. Call destroy first
    2. The destructor shall be called after the DestroyCb
    is returned.
    
    Change-Id: Ib09ab30944cc7ad2c87aa52bd13b76c1e51186ba
    CRs-Fixed: 4071786

--- integration_api/inc/LocationIntegrationApi.h
+++ integration_api/inc/LocationIntegrationApi.h
@@ -26,38 +26,9 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
-Changes from Qualcomm Innovation Center are provided under the following license:
-Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted (subject to the limitations in the
-disclaimer below) provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-
-    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
 #ifndef LOCATION_INTEGRATION_API_H
@@ -568,6 +539,12 @@ typedef std::function<void(
 typedef std::function<void(
    uint8_t minSvElevation
 )> LocConfigGetMinSvElevationCb;
+
+/** @brief Callback to confirm the the LocationIntegrationApi
+           instance is destroyed.<br/>
+*/
+typedef std::function<void(
+)> LocIntegrationDestroyCb;
 
 /** @brief
     LocConfigGetConstellationSecondaryBandConfigCb is for
@@ -2060,6 +2037,16 @@ public:
                 <br/>
     */
     bool setUserConsentForXtra(bool userConsent);
+
+
+    /** @brief
+       Destroy/cleans up the instance of LocationIntegrationApi object,
+       which should be called when LocationIntegrationApi object is
+       no longer needed.
+       The caller shall not destruct LocationIntegrationApi object before
+       destroyCompleteCb is invoked
+    */
+    void destroy(LocIntegrationDestroyCb destroyCompleteCb);
 
     /** @example example1:testGetConfigApi
     * <pre>
