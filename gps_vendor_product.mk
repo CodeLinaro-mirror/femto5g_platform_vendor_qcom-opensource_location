16e2fa65613b7a72595acfbf01648d826574098b
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2025-10-20 11:17:33 -0700

    libgnsspps: remove libpps as the functionality is no longer used
    As we have switched to user qtimer for GPS time alignment, so
    PPS-based time alignment is no longer needed
    
    CRs-fixed: 4327444
    
    Change-Id: I38cb57eef957c16436bb1bb77ac0b7802bd343e5

--- gps_vendor_product.mk
+++ gps_vendor_product.mk
@@ -2,7 +2,6 @@
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 
 PRODUCT_PACKAGES += libloc_api_v02
-PRODUCT_PACKAGES += libgnsspps
 PRODUCT_PACKAGES += libsynergy_loc_api
 PRODUCT_PACKAGES += libloc_socket
 PRODUCT_PACKAGES += libloc_base_util
