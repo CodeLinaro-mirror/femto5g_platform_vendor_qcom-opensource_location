c60cc8f9defaeda6557fd05ce32a314fe09c9d49
Santoshkumar Zalake <szalake@qti.qualcomm.com>
2026-02-12 15:27:18 +0530

    [FIDL-Loc] Enable locIdlClient based on SOMEIP
    The locIdlClient enabled based on
    TARGET_SOMEIP_ENABLE
    
    CRs-Fixed: 4439256
    Change-Id: Ifdcb806c7b3f6dde3f173205c478704863b8a740

--- gps_vendor_product.mk
+++ gps_vendor_product.mk
@@ -8,6 +8,11 @@ PRODUCT_PACKAGES += izat_remote_api_headers
 PRODUCT_PACKAGES += loc_sll_if_headers
 PRODUCT_PACKAGES += libloc_socket
 
+PRODUCT_PACKAGES += liblocation_api_msg
+PRODUCT_PACKAGES += liblocation_integration_api
+PRODUCT_PACKAGES += liblocation_client_api
+
+ifneq ($(TARGET_SOMEIP_ENABLE), false)
 PRODUCT_PACKAGES += liblocClient_fidl_if_headers
 PRODUCT_PACKAGES += liblocApiV02_fidl_headers
 PRODUCT_PACKAGES += libfidl_loc_api
@@ -16,13 +21,10 @@ PRODUCT_PACKAGES += libloc_fidl_sim
 PRODUCT_PACKAGES += libLocationFidlV02
 PRODUCT_PACKAGES += locIdlClient_daemon
 
-PRODUCT_PACKAGES += liblocation_api_msg
-PRODUCT_PACKAGES += liblocation_integration_api
-PRODUCT_PACKAGES += liblocation_client_api
-
 PRODUCT_PACKAGES += commonapi4someip.ini
 PRODUCT_PACKAGES += vsomeip-client.json
 PRODUCT_PACKAGES += libLocIdlAPI-proxy
 PRODUCT_PACKAGES += libLocIdlAPI-someip
 PRODUCT_PACKAGES += LocIdlAPIClient
+endif#TARGET_SOMEIP_ENABLE
 endif#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
